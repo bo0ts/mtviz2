aef5ac2737bfe753edfc72a32ebc66c2c99b8b4a
Philipp Möller <bootsarehax@googlemail.com>
2011-10-23 14:38:07 +0200

    improved styling and interface

--- utility.js
+++ utility.js
@@ -1,3 +1,9 @@
+var modes = {
+    NONE: 0,
+    RING: 1,
+    BAR: 2
+}, current_mode = modes[modes.NONE];
+
 function switch_mode(mode) {
     // remove the old svg
     var fig = document.getElementById("fig");
@@ -10,14 +16,14 @@ function switch_mode(mode) {
         .width(conf.width + conf.margin)
         .height(conf.width + conf.margin)
         .canvas('fig');
-    
+
     // update the reference in the config
     conf.panel = root;
 
     if(mode == "ring") {
         vis = new WedgeVis(root, data);
     }
-    
+
     if(mode == "bar") {
         vis = new BarVis(root, data);
     }
@@ -35,7 +41,7 @@ function exportSVG() {
     else {
         builder = new window.WebKitBlobBuilder();
     }
-    
+
     // look for the svg and bang the string repl in builder
     var fig = document.getElementById("fig");
 
@@ -49,6 +55,17 @@ function hide_sub(e) {
     //magic, only do it when we came from li
     e = e.parentNode;
     var ul = e.getElementsByTagName("ul");
+    // swap the arrow div
+    var div = e.getElementsByTagName("div");
+
+    if(div.length > 0) {
+        var attr = div[0].getAttribute('class');
+        if(attr === 'arrow-right')
+            div[0].setAttribute('class', 'arrow-down');
+        else
+            div[0].setAttribute('class', 'arrow-right');
+    }
+
     hide(ul[0]);
 }
 
@@ -57,9 +74,20 @@ function hide(e) {
 
     if(e.style.visibility != "hidden") {
         e.style.visibility = "hidden";
-        e.style.display = "none"; 
+        e.style.display = "none";
     } else {
         e.style.visibility = "visible";
-        e.style.display = "inline"; 
+        e.style.display = "inline";
     }
+}
+
+
+function getTextWidth(text, fstyle) {
+    var e = document.createElement("div");
+    e.style.position = "absolute";
+    e.style.width = "auto";
+    e.style.
+    e.innerHTML = text;
+    document.appendChild(e);
+    return e.clientWidth;
 }
